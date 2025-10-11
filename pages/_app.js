5e95767a8df576bf248798f01b0293e1de420cb2
muzamono <m.zakuan7575@gmail.com>
2025-10-11 16:16:54 +0800

    Revert to local-only TinaCMS mode

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,5 @@
 import "@/styles/globals.css";
-import dynamic from "next/dynamic";
-
-// Dynamically import TinaCMS provider (no SSR)
-const TinaProvider = dynamic(() => import("../.tina/__generated__/TinaCMS"), {
-  ssr: false,
-});
 
 export default function App({ Component, pageProps }) {
-  return (
-    <TinaProvider>
-      <Component {...pageProps} />
-    </TinaProvider>
-  );
+  return <Component {...pageProps} />;
 }
--- tina/config.js
+++ tina/config.js
@@ -26,7 +26,7 @@ export default defineConfig({
   },
   // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
 
-  },
+  
   schema: {
     collections: [
       {
